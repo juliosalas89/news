c1a3b6a4db4acf210754976604bb72d28d5680da
Julio Salas <julio.a.salas89@gmail.com>
2022-05-06 18:35:04 +0200

    guardo para instalar una cosa que puede romper todo, y commiteo a DEV

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from './components/generales/Header';
+import Footer from './components/generales/Footer';
 
 function App() {
   return (
     <div>
       <Header></Header>
+      <Footer></Footer>
     </div>
   );
 }
