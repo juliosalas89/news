ad09143d91b48d78f5583ddcba112b496ea4da69
Julio Salas <julio.a.salas89@gmail.com>
2022-06-03 13:32:57 +0200

    se termina con abm de categorias

--- src/components/administrador/ListaCategorias.js
+++ src/components/administrador/ListaCategorias.js
@@ -5,22 +5,58 @@ import Table from 'react-bootstrap/Table';
 import Button from 'react-bootstrap/Button';
 import ModalCategoria from './ModalCategoria';
 import mayusculas from '../helpers/mayusculas';
-
+import Swal from 'sweetalert2';
 
 const ListaCategorias = (props) => {
     const [mostrarModal, setMostrarModal] = useState(false);
     const [categoriaEditar, setCategoriaEditar] = useState();
     const [editar, setEditar] = useState(false);
 
-    const eliminarCategoria = () => {
-
-    }
-
     const editarCategoria = (id)=> {
         const categoriaEncontrada = props.categorias.find(categoria => categoria._id === id);
         setCategoriaEditar(categoriaEncontrada);
         setEditar(true);
         abrirModal();
+    }
+
+    const eliminarCategoria = async (id) => {
+        const categoriaEncontrada = props.categorias.find(categoria => categoria._id === id);
+            Swal.fire({
+                title: `¿Seguro que quieres eliminar esta categoria?`,
+                text: `${mayusculas(categoriaEncontrada.nombre)}: ${categoriaEncontrada.descripcion}.`,
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Si, borrar categoría!'
+            }).then(async (result) => {
+                if (result.isConfirmed) {
+                    try {
+                        const header = {
+                            method: 'DELETE',
+                            headers: {
+                                'Content-Type': 'application/json'
+                            }
+                        }
+                        const respuesta = await fetch(`https://noticias-back.herokuapp.com/api/categorias/${id}`, header)
+                        if (respuesta.status === 200) {
+                            Swal.fire(
+                                'Barrada!',
+                                'La categoría fué eliminada de la base de datos',
+                                'success'
+                            )
+                            props.setConsultarBack(true);
+                        }
+                    } catch (error) {
+                        console.log(error);
+                        Swal.fire(
+                            'oops!',
+                            'Algo salió mal.. intentalo de nuevo',
+                            'error'
+                        )
+                    }
+                }
+            })
     }
 
     const abrirModal = () => setMostrarModal(true);
--- src/components/administrador/ModalCategoria.js
+++ src/components/administrador/ModalCategoria.js
@@ -12,22 +12,46 @@ const ModalCategoria = (props) => {
     const [validDescripcion, setValidDescripcion] = useState(false);
     const [errorBD, setErrorBD] = useState(false);
 
-    useEffect(()=> {
+    useEffect(() => {
+        resetValores()
+        //eslint-disable-next-line
+    }, [props.mostrarModal])
+
+    useEffect(() => {
+        cargarDatosEnForm();
+        //eslint-disable-next-line
+    }, [props.editar])
+
+    const cerrarModal = () => props.setMostrarModal(false);
+
+    const resetValores = () => {
         setValidNombre(false);
         setValidDescripcion(false);
         setDescripcion("");
         setNombre("");
         setErrorBD(false);
         setDestacada(false);
-        if(props.mostrarModal === false) {
+        if (props.mostrarModal === false) {
             props.setEditar(false)
         }
-        //eslint-disable-next-line
-    }, [props.mostrarModal])
+    }
 
-    const cerrarModal = () => props.setMostrarModal(false);
+    const cargarDatosEnForm = () => {
+        if (props.editar) {
+            setNombre(props.categoriaEditar.nombre);
+            document.getElementById("nombre").value = props.categoriaEditar.nombre;
+            setDescripcion(props.categoriaEditar.descripcion);
+            document.getElementById("descripcion").value = props.categoriaEditar.descripcion;
+            setDestacada(props.categoriaEditar.destacada);
+            if (props.categoriaEditar.destacada) {
+                document.getElementById("checkbox").checked = true;
+            }
+            document.getElementById("boton").innerText = "Guardar cambios"
+        } else {
+        }
+    }
 
-    const destacarCategoria = ()=> {
+    const destacarCategoria = () => {
         if (document.getElementById('checkbox').checked) {
             setDestacada(true);
         } else {
@@ -35,8 +59,8 @@ const ModalCategoria = (props) => {
         }
     }
 
-    const agregarCategoria = async ()=> {
-        if(validarFormulario()) {
+    const handleClick = async () => {
+        if (validarFormulario()) {
             return
         }
         const datos = {
@@ -44,41 +68,68 @@ const ModalCategoria = (props) => {
             descripcion,
             destacada
         }
-        try {
-            const header = {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(datos)
+        if (props.editar) {
+            try {
+                const header = {
+                    method: 'PUT',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(datos)
+                }
+                const respuesta = await fetch(`https://noticias-back.herokuapp.com/api/categorias/${props.categoriaEditar._id}`, header);
+                console.log(respuesta)
+                if (respuesta.status === 200) {
+                    props.setMostrarModal(false);
+                    props.setConsultarBack(true);
+                    Swal.fire(
+                        'Categoria editada!',
+                        'La categoría se editó correctamente',
+                        'success'
+                    )
+                } else {
+                    setErrorBD(true);
+                }
+            } catch (error) {
+                console.log(error);
             }
-            const respuesta = await fetch("https://noticias-back.herokuapp.com/api/categorias", header);
-            console.log(respuesta)
-            if(respuesta.status === 201) {
-                props.setMostrarModal(false);
-                props.setConsultarBack(true);
-                Swal.fire(
-                    'Categoria creada!',
-                    'La nueva categoría se guardo correctamente',
-                    'success'
-                )
-            } else {
-                setErrorBD(true);
+        } else {
+            try {
+                const header = {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(datos)
+                }
+                const respuesta = await fetch("https://noticias-back.herokuapp.com/api/categorias", header);
+                console.log(respuesta)
+                if (respuesta.status === 201) {
+                    props.setMostrarModal(false);
+                    props.setConsultarBack(true);
+                    Swal.fire(
+                        'Categoria creada!',
+                        'La nueva categoría se guardo correctamente',
+                        'success'
+                    )
+                } else {
+                    setErrorBD(true);
+                }
+            } catch (error) {
+                console.log(error);
             }
-        } catch(error) { 
-            console.log(error);
         }
     }
 
-    const validarFormulario = ()=> {
+    const validarFormulario = () => {
         let validacion = false;
-        if(nombre.trim() === "" ) {
+        if (nombre.trim() === "") {
             setValidNombre(true);
             validacion = true;
         } else {
             setValidNombre(false);
         }
-        if(descripcion.trim() === "" ) {
+        if (descripcion.trim() === "") {
             setValidDescripcion(true);
             validacion = true;
         } else {
@@ -90,45 +141,43 @@ const ModalCategoria = (props) => {
     return (
         <Modal show={props.mostrarModal} onHide={cerrarModal} animation={false}>
             <Modal.Header closeButton>
-                <Modal.Title>Nueva Ctaegoria</Modal.Title>
+                {props.editar ? <Modal.Title>Editar Ctaegoria</Modal.Title> : <Modal.Title>Nueva Ctaegoria</Modal.Title>}
+
             </Modal.Header>
             <Modal.Body>
                 <Form>
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Group className="mb-3">
                         <Form.Label>Nombre de la categoria</Form.Label>
-                        <Form.Control type="text" onChange={(e) => setNombre(e.target.value)} placeholder="Enter email" />
-                        { validNombre ?
-                            <Form.Text className="text-danger">
-                                Debe introducir un nombre
-                            </Form.Text> : null
-                        }
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="formBasicPassword">
-                        <Form.Label>Descripción</Form.Label>
-                        <Form.Control type="text" as="textarea" onChange={(e) => setDescripcion(e.target.value)} placeholder="Escriba aquí una descripción del contenido que abarca esta categoría" />
-                        { validDescripcion ?
+                        <Form.Control type="text" id="nombre" onChange={(e) => setNombre(e.target.value)} placeholder="Enter email" />
+                        {validNombre ?
                             <Form.Text className="text-danger">
                                 Debe introducir un nombre
                             </Form.Text> : null
                         }
                     </Form.Group>
                     <Form.Group className="mb-3">
-                        <Form.Check id='checkbox' onChange={()=> destacarCategoria()} type="checkbox" label="Categoria destacada." />
+                        <Form.Label>Descripción</Form.Label>
+                        <Form.Control type="text" as="textarea" id="descripcion" onChange={(e) => setDescripcion(e.target.value)} placeholder="Escriba aquí una descripción del contenido que abarca esta categoría" />
+                        {validDescripcion ?
+                            <Form.Text className="text-danger">
+                                Debe introducir un nombre
+                            </Form.Text> : null
+                        }
                     </Form.Group>
-                    <Button variant="primary" type="submit">
-                        Submit
-                    </Button>
+                    <Form.Group className="mb-3">
+                        <Form.Check id='checkbox' onChange={() => destacarCategoria()} type="checkbox" label="Categoria destacada." />
+                    </Form.Group>
                 </Form>
             </Modal.Body>
-            {   errorBD ?
-                <Form.Text className='ms-3 text-danger fs-5'>Algo salio mal... Intentalo nuevamente</Form.Text> 
+            {errorBD ?
+                <Form.Text className='ms-3 text-danger fs-5'>Algo salio mal... Intentalo nuevamente</Form.Text>
                 : null}
             <Modal.Footer>
                 <Button variant="secondary" onClick={() => cerrarModal()}>
                     Cerrar
                 </Button>
-                <Button variant="primary" onClick={() => agregarCategoria()}>
-                    Guardar Categoria
+                <Button variant="primary" id="boton" onClick={() => handleClick()}>
+                    Agregar categoria
                 </Button>
             </Modal.Footer>
         </Modal>
